8cfc8c4af2e79d35b8052535fd724fc14df844fb
kimjihun <66763460+kimjihun-dev@users.noreply.github.com>
2020-11-10 14:29:14 +0900

    Update index.vue

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -4,7 +4,9 @@
             <div class="main-bg">
                 <div class="welcome">
                     <p class="txt">kimjihun portfolio.</p>
-                    <a href="/portfolio" class="btnA">view site</a>
+                    <span>
+                        포트폴리오들은 상업적인 아닌 개인 포트폴리용입니다.
+                    </span>
                 </div>
             </div>
         </div>
@@ -24,11 +26,10 @@ export default {
 <style>
 .main-bg { max-width: 100%; width: 100%; height: 550px; background: url('~assets/images/main-bg.jpg') no-repeat center/cover; }
 .welcome { padding-top: 50%; transform: translateY(-30%); padding-left: 35px; }
-.welcome .txt { font-size: 25px; margin-bottom: 20px; font-weight: bold; color: #fff; text-transform: uppercase;  }
-.welcome .btnA { color: rgb(206, 206, 206); font-size: 14px; text-transform: uppercase; position: relative; }
-.welcome .btnA::after { position: absolute; content: ""; width: 27px; top: 5px; right: -33px; height: 6px; background: url('~assets/images/arrow-btn.png') no-repeat; }
+.welcome .txt { font-size: 25px; margin-bottom: 10px; font-weight: bold; color: #fff; text-transform: uppercase;  }
+.welcome span { font-size: 14px; color: #ddd; }
 @media (max-width: 768px) {
     .welcome { padding-top: 0; transform: translateY(-0%); padding-left: 35px; }
-    .welcome { padding-top: 70%; }
+    .welcome { padding-top: 50%; }
 }
 </style>
